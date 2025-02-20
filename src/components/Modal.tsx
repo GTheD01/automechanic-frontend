37a6f17cbd6a8572d42ac0c709445fac3c60dbb6
GTheD01 <popeftimovgeorgi@yahoo.com>
2025-02-20 14:27:23 +0100

    Added modal transition on open

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,5 @@
-import { PropsWithChildren, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
+import { PropsWithChildren, useEffect, useRef, useState } from "react";
 
 interface ModalProps {
   open: boolean;
@@ -13,12 +13,16 @@ function Modal({
   onClose,
   className,
 }: PropsWithChildren<ModalProps>) {
+  const [isTransitioning, setIsTransitioning] = useState<boolean>(false);
   const dialog = useRef<HTMLDialogElement | null>(null);
 
   useEffect(() => {
     const modal = dialog.current;
     if (open) {
+      setIsTransitioning(true);
       modal?.showModal();
+    } else {
+      setIsTransitioning(false);
     }
 
     return () => modal?.close();
@@ -35,7 +39,14 @@ function Modal({
       ref={dialog}
       onClose={onClose}
       onClick={handleBackdropClick}
-      className={`backdrop:bg-black/60 border-none p-4 min-w-80 w-[30%] rounded-lg shadow-lg ${className}`}
+      className={`backdrop:bg-black/60 border-none p-4 min-w-80 w-[30%] rounded-lg shadow-lg ${className}
+        ${
+          isTransitioning
+            ? "opacity-100 -translate-y-0"
+            : "opacity-0 -translate-y-10"
+        } 
+        transition-all duration-300
+      `}
     >
       {open ? children : null}
     </dialog>,
