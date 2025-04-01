7fdc8ab483e2431cf18ca9440cabf81bf6e05bf6
GTheD01 <popeftimovgeorgi@yahoo.com>
2025-04-01 16:03:05 +0200

    Add Axios interceptor to handle access token refresh on expiration

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,4 +5,25 @@ const apiClient = axios.create({
   withCredentials: true,
 });
 
+const refreshAccessToken = async () => {
+  const response = await apiClient.post("/auth/jwt/refresh-token");
+  return response.data;
+};
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response.status === 401) {
+      try {
+        await refreshAccessToken();
+
+        return apiClient(error.config);
+      } catch (err) {
+        console.error("Unable to refresh token", err);
+        return Promise.reject(error);
+      }
+    }
+  }
+);
+
 export default apiClient;
